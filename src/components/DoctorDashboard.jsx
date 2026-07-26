5d126951708ad90721f522bb288c34a836c480d8
Harsh <apple@Harshs-MacBook-Pro.local>
2026-07-26 21:05:35 +0530

    Fix React state preservation bug in doctor settings to prevent inputs resetting on re-render

--- src/components/DoctorDashboard.jsx
+++ src/components/DoctorDashboard.jsx
@@ -40,7 +40,7 @@ export default function DoctorDashboard() {
       setDocSlots(activeDoctor.slotsPerDay || 8);
       setDocSlotsList(activeDoctor.availableSlots || ['09:00 AM - 10:00 AM', '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 01:00 PM', '02:00 PM - 03:00 PM', '03:00 PM - 04:00 PM', '04:00 PM - 05:00 PM']);
     }
-  }, [selectedDoctorId, activeDoctor]);
+  }, [selectedDoctorId]);
 
   const handleSaveSchedule = (e) => {
     e.preventDefault();
