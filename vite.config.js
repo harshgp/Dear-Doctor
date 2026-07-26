a5cf0cccebb0ea00bc965528cc0f04351bbc40c3
Harsh <apple@Harshs-MacBook-Pro.local>
2026-07-26 20:37:35 +0530

    Configure Vite root build and push production files directly to main branch

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+    assetsDir: 'assets'
+  },
   base: './'
 })
